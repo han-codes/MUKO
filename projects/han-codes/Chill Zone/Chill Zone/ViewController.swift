5a9935a3ef8aec0e4690a177dc79f204424b5d5e
Han Kim <han.kim@metova.com>
2020-04-12 22:44:50 -0500

    Initial Xcode project setup

---
+++ projects/han-codes/Chill Zone/Chill Zone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chill Zone
+//
+//  Created by Hannie Kim on 4/12/20.
+//  Copyright © 2020 Hannie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
